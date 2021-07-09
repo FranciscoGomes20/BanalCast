2b8555e143d761b94807f7ca32758692c18c4240
FranciscoGomes20 <henriquecgomes10@gmail.com>
2021-07-09 09:27:03 -0500

    Alteração nos templates 404 e 500

--- BanalCast/views.py
+++ BanalCast/views.py
@@ -4,7 +4,7 @@ from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
-    postes = Post.objects.all().reverse()
+    postes = Post.objects.all().reverse()[:10]
     quantidade_poste = len(postes)
 
     paginator = Paginator(postes, 5)
